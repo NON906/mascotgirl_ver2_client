4c89d30c5d2132ec48b6ccf14d55b2c86c1e74eb
NON906 <mumeigames@gmail.com>
2024-11-13 12:41:30 +0900

    キャラクターのリサイズを修正

--- MascotGirl_Client_ver2/Assets/MascotGirl_Client/Scripts/CharaQuadSize.cs
+++ MascotGirl_Client_ver2/Assets/MascotGirl_Client/Scripts/CharaQuadSize.cs
@@ -25,14 +25,7 @@ namespace MascotGirlClient
 
             var scale = Mathf.Tan(TargetCamera.fieldOfView * Mathf.Deg2Rad * 0.5f) * (transform.position - TargetCamera.transform.position).magnitude * 2f;
             var aspect = (float)texture.width / texture.height;
-            if (aspect < TargetCamera.aspect)
-            {
-                transform.localScale = scale * new Vector3(aspect, 1f, 1f);
-            }
-            else
-            {
-                transform.localScale = scale * new Vector3(1f, 1f / aspect, 1f);
-            }
+            transform.localScale = scale * new Vector3(aspect, 1f, 1f);
         }
     }
 }
